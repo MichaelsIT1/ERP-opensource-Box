121e1fbd39f27a11d6ae4ee927d889d313143ac8
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-01-23 11:10:27 +0100

    Update 01-lxc-container-erzeugen.sh

--- 01-lxc-container-erzeugen.sh
+++ 01-lxc-container-erzeugen.sh
@@ -3,9 +3,9 @@
 # Nur fuer Testzwecke
 # Version 0.1
 
-# Variable
-# Container-ID
-CID=999
+# Variablen
+CID=999         #Container-ID
+CPW=12345       #Container root-Passwort
 
 clear
 
@@ -17,12 +17,12 @@ pct create $CID local:vztmpl/debian-11-standard_11.0-1_amd64.tar.gz \
         -memory 4096 \
         -net0 name=eth0,bridge=vmbr0,ip=dhcp \
         -unprivileged 1 \
-        -password 12345 \
+        -password $CPW \
         -features nesting=1
 
 pct start $CID
 sleep 10
 
-pct exec $CID -- bash -c "apt install git -y && git clone https://github.com/MichaelsIT1/ERP-opensource-Box.git"
-
-lxc-attach -n$CID bash /root/ERP-opensource-Box/install-xentral-opensource.sh
+# Installation xentral opensource
+pct push $CID install-xentral-opensource.sh /root/install-xentral-opensource.sh
+pct exec $CID -- bash -c "sh /root/install-xentral-opensource.sh"
