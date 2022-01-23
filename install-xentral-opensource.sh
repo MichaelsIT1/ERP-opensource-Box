11d113acec6442599e3ba6461d9bca0bd17dd14e
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-01-23 14:38:02 +0100

    Update install-xentral-opensource.sh

--- install-xentral-opensource.sh
+++ install-xentral-opensource.sh
@@ -43,8 +43,8 @@ then
         sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | mysql_secure_installation
                     # current root password (emtpy after installation)
         y           # Set root password?
-        test1234    # new root password
-        test1234    # new root password         y           # Remove anonymous users?
+        xentral     # new root password
+        xentral     # new root password         y           # Remove anonymous users?
         y           # Disallow root login remotely?
         y           # Remove test database and access to it?
         y           # Reload privilege tables now?
