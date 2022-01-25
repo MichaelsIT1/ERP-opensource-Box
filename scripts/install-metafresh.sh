ed4366b7ada20a1ea4d10ef9afda2e8fadc291e2
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-01-25 11:19:57 +0100

    Update install-metafresh.sh

--- scripts/install-metafresh.sh
+++ scripts/install-metafresh.sh
@@ -6,7 +6,6 @@
 
 # System-Varibale
 IP=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
-
 clear
 echo "metafresh installieren"
 echo "*******************************"
@@ -23,15 +22,24 @@ echo "Docker wird installiert"
 echo "**************************************************"
 apt install docker.io docker-compose git -y
 echo
+sleep 5
 echo "metafresh image herunterladen"
 echo "********************************"
 git clone https://github.com/metasfresh/metasfresh-docker.git
 cd metasfresh-docker/
 echo
+sleep 2
+
+sed -i "s|#environment|environment|g" docker-compose.yml #environment auskommentieren
+sed -i "s|#- WEBAPI_URL=http://example.com:8080|- WEBAPI_URL=http://$(hostname -f)|g" docker-compose.yml #hostname eintragen
+
+sleep 2
 echo "Container bauen"
 echo "***********************************************************"
 docker-compose build
+sleep 5
 docker-compose up -d
+
 echo "*******************************************************************************************"
 echo "metafreh installiert. Bitte ueber das Web die Konfiguration vornehmen"
 echo "weiter gehts mit dem Browser. Gehen Sie auf http://$IP/"
