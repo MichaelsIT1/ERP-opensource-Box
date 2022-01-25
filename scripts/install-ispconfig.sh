366247250c93cd5fc8a6964eea161a79b39a6972
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-01-25 20:04:30 +0100

    Update install-ispconfig.sh

--- scripts/install-ispconfig.sh
+++ scripts/install-ispconfig.sh
@@ -21,7 +21,7 @@ apt update && apt dist-upgrade -y
 echo
 echo "ISP-Config wird installiert"
 echo "**************************************************"
-wget -O - https://get.ispconfig.org | sh -s -- --use-ftp-ports=40110-40210 --unattended-upgrades --i-know-what-i-am-doing --no-quota
+wget -O - https://get.ispconfig.org | sh -s -- --use-ftp-ports=40110-40210 --unattended-upgrades --i-know-what-i-am-doing --no-quota --use-php=8.0,8,1
 echo "**************************************************************************"
 echo "weiter gehts mit dem Browser. Gehen Sie auf https://$IP:8080"
 echo "**************************************************************************"
