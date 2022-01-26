43b3d68f583d5ab24da5cb39ff68fc1bcc8fc619
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-01-26 12:18:39 +0100

    Update install-portainer.sh

--- scripts/install-portainer.sh
+++ scripts/install-portainer.sh
@@ -28,6 +28,7 @@ sleep 5
 
 # Portainer herunterladen und starten
 docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
-    
+sleep 10
+
 echo "weiter gehts mit dem Browser. Gehen Sie auf http://$IP:9000/"
 echo "*************************************************************"
