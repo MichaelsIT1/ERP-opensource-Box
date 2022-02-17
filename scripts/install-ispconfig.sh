378b05630e237e603a569282e393dc7425c8669f
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-02-17 14:20:07 +0100

    Update install-ispconfig.sh

--- scripts/install-ispconfig.sh
+++ scripts/install-ispconfig.sh
@@ -15,7 +15,7 @@ PHPMYADMIN=true
 
 IP=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
 HOSTNAME_NAME=$HOSTNAME
-HOSTNAME_DNSNAME=$(HOSTNAME -f)
+HOSTNAME_DNSNAME=($HOSTNAME -f)
 
 # Shell auf bash stellen
 echo "dash dash/sh boolean false" | debconf-set-selections && DEBIAN_FRONTEND=noninteractive dpkg-reconfigure dash 2>&1
