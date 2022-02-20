77d535f596e601b0c4070e9f9045661e91359b99
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-02-20 19:39:57 +0100

    Update install-ispconfig.sh

--- scripts/install-ispconfig.sh
+++ scripts/install-ispconfig.sh
@@ -114,10 +114,40 @@ sleep 30
 
 mysql -u root -e "SET PASSWORD FOR root@'localhost' = PASSWORD('$MARIADB_PW');"
 
+
+# Make sure that NOBODY can access the server without a password
+mysql -e "UPDATE mysql.user SET Password = PASSWORD('$MARIADB_PW') WHERE User = 'root'"
+# Kill the anonymous users
+mysql -e "DROP USER ''@'localhost'"
+# Because our hostname varies we'll use some Bash magic here.
+mysql -e "DROP USER ''@'$(hostname)'"
+# Kill off the demo database
+mysql -e "DROP DATABASE test"
+# Make our changes take effect
+mysql -e "FLUSH PRIVILEGES"
+# Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 sleep 3
 
 # MariaDB-Passwort setzen
 sed -i "s|user     = root|user     = root\npassword = $MARIADB_PW|g" /etc/mysql/debian.cnf
+
 systemctl restart mariadb 
 sleep 5
 
