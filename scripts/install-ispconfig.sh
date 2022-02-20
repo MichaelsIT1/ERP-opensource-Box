5bf21bdc41dc32c131c3a38b6ff1c819d8d69199
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-02-20 14:38:06 +0100

    Update install-ispconfig.sh

--- scripts/install-ispconfig.sh
+++ scripts/install-ispconfig.sh
@@ -22,7 +22,7 @@ FIREWALL=false
 
 IP=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
 HOSTNAME_NAME=$HOSTNAME
-HOSTNAME_DNSNAME=dnsdomainname
+HOSTNAME_DNSNAME=$(hostname -f)
 sleep 3
 
 # Shell auf bash stellen
