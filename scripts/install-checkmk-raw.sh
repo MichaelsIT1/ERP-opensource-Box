a0717f34dd711c1d43418d9cf8245c3e54d8376a
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-02-21 22:14:57 +0100

    Create install-checkmk-raw.sh

---
+++ scripts/install-checkmk-raw.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# Status: Alpha
+# Nur fuer Test geeignet. Nicht fuer den produktiven Einsatz.
+# getestet auf Debian 11 im LXC Container
+
+# System-Varibale
+IP=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
+
+clear
+echo "checkMK installieren"
+echo "*******************************"
+echo
+echo "Zeitzone auf Europe/Berlin gesetzt"
+echo "**********************************"
+timedatectl set-timezone Europe/Berlin 
+echo
+echo "Betriebssystem debian wird aktualisiert"
+echo "***************************************"
+apt update && apt dist-upgrade -y
+echo
+echo "Webserver Apache, MariaDB und PHP wird installiert"
+echo "**************************************************"
+apt install apache2 mariadb-server php php-mbstring php-soap php-imap php-xml php-zip php-gd php-cli php-mysql php-curl php-ldap unzip zip graphviz -y
+echo
+
+
+cd /root/
+wget https://download.checkmk.com/checkmk/2.0.0p20/check-mk-raw-2.0.0p20_0.bullseye_amd64.deb
+
+apt install ./check*
+
+omd create TEST
+omd start TEST
+
+systemctl restart apache2
+
+echo "*******************************************************************************************"
+echo "checkmk raw erfolgreich installiert. Bitte ueber das Web die Konfiguration vornehmen"
+echo "weiter gehts mit dem Browser. Gehen Sie auf http://$IP/TEST/"
+echo "**************************************************************************"
