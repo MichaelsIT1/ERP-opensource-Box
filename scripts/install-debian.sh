34b1679e6c8e133a1a4184b27847f307a18d5507
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-02-22 21:45:46 +0100

    Update install-debian.sh

--- scripts/install-debian.sh
+++ scripts/install-debian.sh
@@ -12,9 +12,3 @@ EOF
 echo "Betriebssystem wird aktualisiert"
 echo "***************************************"
 apt update -y && apt dist-upgrade -y
-echo
-sleep 3
-
-wget -O - https://get.ispconfig.org | sh -s -- --help
-
-wget -O - https://get.ispconfig.org | sh -s -- --use-php=7.4 --no-dns --no-mailman --no-quota --no-ntp --unattended-upgrades
