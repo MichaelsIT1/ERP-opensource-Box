686ed4d6dfde3b8a9b1fba8fbd4904d883b889d0
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-02-23 07:30:04 +0100

    Update install-ispconfig.sh

--- scripts/install-ispconfig.sh
+++ scripts/install-ispconfig.sh
@@ -343,8 +343,14 @@ fi
 mv /etc/postfix/main.cf /etc/postfix/main.cf_original
 sleep 3
 
-#tee /etc/postfix/main.cf >/dev/null <<EOF
+#tee -a /etc/postfix/main.cf >/dev/null <<EOF
+#smtpd_sender_restrictions = check_sender_access proxy:mysql:/etc/postfix/mysql-virtual_sender.cf, permit_mynetworks, permit_sasl_authenticated, reject_non_fqdn_sender, reject_unlisted_sender
 #myhostname = $HOSTNAME_NAME
+#smtpd_milters = inet:localhost:11332
+#non_smtpd_milters = inet:localhost:11332
+#milter_protocol = 6
+#milter_mail_macros = i {mail_addr} {client_addr} {client_name} {auth_authen}
+#milter_default_action = accept
 #EOF
 
 postfix reload
