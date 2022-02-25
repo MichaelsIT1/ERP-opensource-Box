c35d651be80d31eb2c2511c4d4049193376c37d4
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-02-25 18:57:08 +0100

    Update install-nextcloud.sh

--- scripts/install-nextcloud.sh
+++ scripts/install-nextcloud.sh
@@ -20,7 +20,7 @@ apt update && apt dist-upgrade -y
 echo
 echo "Webserver Apache, MariaDB und PHP wird installiert"
 echo "**************************************************"
-apt install apache2 mariadb-server php php-mbstring php-soap php-imap php-xml php-zip php-gd php-cli php-mysql php-curl php-ldap unzip zip vim -y
+apt install apache2 mariadb-server php php-mbstring php-soap php-imap php-xml php-zip php-gd php-cli php-mysql php-curl php-ldap php-intl php-bcmath php-gmp php-imagick unzip zip vim -y
 echo
 
 
@@ -56,6 +56,40 @@ cd /var/www/html/
 # Offline-installer
 wget https://download.nextcloud.com/server/releases/nextcloud-23.0.2.zip
 unzip nextcloud*
+
+
+mv nextcloud/config/config.php nextcloud/config/config.php.org
+
+
+#config/config.php erzeugen
+tee /var/www/html/nextcloud/config/config.php >/dev/null <<EOF
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocmp60d6dazq',
+  'passwordsalt' => 'n6hcplI/UzRHP9kRLM1gL4ISHQcEY0',
+  'secret' => 'f1q9NmHPPDxriS8D0zBE1mdi1NkvsEZT5xUHrjnblaImuoDi',
+  'trusted_domains' =>
+  array (
+    0 => '$IP',
+  ),
+  'datadirectory' => '/var/www/html/nextcloud/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '23.0.2.1',
+  'overwrite.cli.url' => 'http://192.168.188.89/nextcloud/nextcloud',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'nextcloud',
+  'default_language' => 'de',
+  'default_locale' => 'de_DE',
+  'default_phone_region' => 'DE',
+  'installed' => true,
+);
+EOF
+
 
 
 
