cae2d817b3967b5a74f0823fb0422ae6255c7c30
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-02-28 23:47:03 +0100

    Update install-motioneye.sh

--- scripts/install-motioneye.sh
+++ scripts/install-motioneye.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+# Status: Alpha
+# Nur fuer Test geeignet. Nicht fuer den produktiven Einsatz.
+# getestet auf Debian 11 im LXC Container
+
+# System-Varibale
+IP=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
+
 echo "Zeitzone auf Europe/Berlin gesetzt"
 echo "**********************************"
 timedatectl set-timezone Europe/Berlin 
