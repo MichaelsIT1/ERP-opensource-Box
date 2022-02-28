cadac0ea8bd1e7399345e0793aeeb35878aa63c4
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-02-28 23:40:58 +0100

    Update install-motioneye.sh

--- scripts/install-motioneye.sh
+++ scripts/install-motioneye.sh
@@ -1,1 +1,38 @@
+echo "Zeitzone auf Europe/Berlin gesetzt"
+echo "**********************************"
+timedatectl set-timezone Europe/Berlin 
+echo
+################################ Update your Debian Installation  ###################################################
+# Non-free aktivieren
+tee /etc/apt/sources.list.d/ispconfig.list >/dev/null <<EOF
+deb http://deb.debian.org/debian/ stable main contrib non-free
+deb-src http://deb.debian.org/debian/ stable main contrib non-free
+EOF
 
+echo "Betriebssystem wird aktualisiert"
+echo "***************************************"
+apt update -y && apt dist-upgrade -y
+
+apt-get install motion ffmpeg v4l-utils -y
+systemctl stop motion
+systemctl disable motion
+
+apt-get install python2 -y
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+python2 get-pip.py
+ 
+apt-get install python-dev-is-python2 python-setuptools curl libssl-dev libcurl4-openssl-dev libjpeg-dev zlib1g-dev libffi-dev libzbar-dev libzbar0 -y
+ 
+pip install motioneye
+ 
+mkdir -p /etc/motioneye
+cp /usr/local/share/motioneye/extra/motioneye.conf.sample /etc/motioneye/motioneye.conf
+ 
+mkdir -p /var/lib/motioneye
+ 
+cp /usr/local/share/motioneye/extra/motioneye.systemd-unit-local /etc/systemd/system/motioneye.service
+systemctl daemon-reload
+systemctl enable motioneye
+systemctl start motioneye
+ 
+pip install motioneye --upgrade
