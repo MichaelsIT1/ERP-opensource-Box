362175943aa3e6ef6d6334033617df976f215194
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-07-07 21:07:05 +0200

    Update 01-start.sh

--- 01-start.sh
+++ 01-start.sh
@@ -39,7 +39,7 @@ Choose an option: "
     read -r ans
     case $ans in
     1)  CNAME="invoice-ninja"
-        COS=$DEBIAN11
+        COS=$UBUNTU
         CID=900
         create_container
         install_ninja
