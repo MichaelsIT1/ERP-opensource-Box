bfdcb8cc5e6522e262e003b0987288e99d98c582
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-07-08 20:10:26 +0200

    Update install-ninja.sh

--- scripts/install-ninja.sh
+++ scripts/install-ninja.sh
@@ -18,12 +18,26 @@ echo "Betriebssystem wird aktualisiert"
 echo "***************************************"
 apt update && apt dist-upgrade -y
 echo
-echo "PHP wird installiert"
-echo "**************************************************"
-apt install php php-fpm php-bcmath php-ctype php-fileinfo php-json php-mbstring php-pdo php-tokenizer php-xml php-curl php-zip php-gmp php-gd php-mysqli mariadb-server mariadb-client curl git vim composer -y
-echo
 
- #automatische Installation
+
+echo "Apache Webserver wird installiert"
+echo "**************************************************"
+apt install -y apache2 apache2-utils
+
+systemctl start apache2
+systemctl enable apache2
+
+chown www-data:www-data /var/www/html/ -R
+
+
+echo "MariaDB Datenbankserver wird installiert"
+echo "**************************************************"
+apt install -y mariadb-server mariadb-client
+
+systemctl start mariadb
+systemctl enable mariadb
+
+#automatische Installation
         sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | mysql_secure_installation
                     # current root password (emtpy after installation)
         y           # Set root password?
@@ -36,52 +50,81 @@ echo
 EOF
 
  mysql -u root <<EOF
-        CREATE DATABASE ninjal;
+        CREATE DATABASE ninja;
         CREATE USER 'ninja'@'localhost' IDENTIFIED BY 'ninja';
         GRANT ALL PRIVILEGES ON ninja . * TO 'ninja'@'localhost';
         FLUSH PRIVILEGES;
 EOF
 
 
+echo "PHP wird installiert"
+echo "**************************************************"
+apt install php php-fpm php-bcmath php-ctype php-fileinfo php-json php-mbstring php-pdo php-tokenizer php-xml php-curl php-zip php-gmp php-gd php-mysqli curl git vim composer -y
+echo
 
-#mkdir -p /etc/nginx/cert
-#openssl req -new -x509 -days 365 -nodes -out /etc/nginx/cert/ninja.crt -keyout /etc/nginx/cert/ninja.key
-#rm /etc/nginx/sites-enabled/default
+apt install -y php7.4 libapache2-mod-php7.4 php7.4-mysql php-common php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline
+
+a2enmod php7.4
+systemctl restart apache2
+
+
+echo "Run PHP-FPM with Apache"
+echo "**************************************************"
+
+a2dismod php7.4
+apt install -y php7.4-fpm
+a2enmod proxy_fcgi setenvif
+
+a2enconf php7.4-fpm
+systemctl restart apache2
+
+echo "Invoice Ninja installieren"
+echo "**************************************************"
+cd /var/www/
+mkdir invoice-ninja && cd invoice-ninja
+wget https://github.com/invoiceninja/invoiceninja/releases/download/v5.4.8/invoiceninja.zip
+unzip invoiceninja.zip
+
+chown www-data:www-data /var/www/invoice-ninja/ -R
+chmod 755 /var/www/invoice-ninja/storage/ -R
+
+a2dismod mpm_prefork
 
 tee /etc/apache2/sites-available/invoice-ninja.conf >/dev/null <<EOF
  <VirtualHost *:80>
-    ServerName invoice.yourdomain.com
-    DocumentRoot /var/www/invoiceninja/public
+    ServerName invoice.$(hostname -f)
+    DocumentRoot /var/www/invoice-ninja/public
 
-    <Directory /var/www/invoiceninja/public>
+    <Directory /var/www/invoice-ninja/public>
        DirectoryIndex index.php
        Options +FollowSymLinks
        AllowOverride All
        Require all granted
     </Directory>
 
-    ErrorLog ${APACHE_LOG_DIR}/invoiceninja.error.log
-    CustomLog ${APACHE_LOG_DIR}/invoiceninja.access.log combined
+    ErrorLog ${APACHE_LOG_DIR}/invoice-ninja.error.log
+    CustomLog ${APACHE_LOG_DIR}/invoice-ninja.access.log combined
 
     Include /etc/apache2/conf-available/php7.4-fpm.conf
 </VirtualHost>
 EOF
+
 
 a2ensite invoice-ninja.conf
 a2enmod rewrite
 systemctl restart apache2
 
 
-cd /var/www/html
-mkdir invoiceninja && cd invoiceninja
-wget https://github.com/invoiceninja/invoiceninja/releases/download/v5.4.8/invoiceninja.zip
-unzip invoiceninja.zip
 
 
-cp .env.example .env
 
-chown -R www-data:www-data /usr/www/html/invoiceninja
-php7.4 artisan optimize
+
+
+
+#cp .env.example .env
+
+
+#php7.4 artisan optimize
 
 #echo "Cronjob wird erzeugt"
 #echo "********************"
@@ -94,4 +137,4 @@ php7.4 artisan optimize
 clear
 echo "*******************************************************************************************"
 echo "Server wurde vorbereitet. Bitte ueber das Web das Setup starten"
-echo "weiter gehts mit dem Browser. Gehen Sie auf http://$IP/"
+echo "weiter gehts mit dem Browser. Gehen Sie auf http://$IP/ oder http://invoice.$(hostname -f)"
