e9842470d4c7faafed0e1f2f693febe86764194f
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-07-10 21:51:16 +0200

    Update install-dewawi.sh

--- scripts/install-dewawi.sh
+++ scripts/install-dewawi.sh
@@ -1,1 +1,20 @@
+#!/bin/sh
+# Status: Alpha
+# Nur fuer Test geeignet. Nicht fuer den produktiven Einsatz.
+# getestet auf Debian 11 im LXC Container
 
+# System-Varibale
+IP=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
+
+clear
+echo "DEWAWI installieren"
+echo "*******************************"
+echo
+echo "Zeitzone auf Europe/Berlin gesetzt"
+echo "**********************************"
+timedatectl set-timezone Europe/Berlin 
+echo
+echo "Betriebssystem wird aktualisiert"
+echo "***************************************"
+apt update && apt dist-upgrade -y
+echo
