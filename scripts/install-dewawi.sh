badfe542f3231957f4ad8238206b53b880722f68
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-07-10 22:27:44 +0200

    Update install-dewawi.sh

--- scripts/install-dewawi.sh
+++ scripts/install-dewawi.sh
@@ -18,3 +18,33 @@ echo "Betriebssystem wird aktualisiert"
 echo "***************************************"
 apt update -y && apt dist-upgrade -y
 echo
+echo "Webserver Apache, MariaDB und PHP wird installiert"
+echo "**************************************************"
+apt install apache2 mariadb-server php php-mbstring php-soap php-imap php-xml php-zip php-gd php-cli php-mysql php-curl unzip zip -y
+echo
+
+
+############# Datenbank erzeugen #########################
+ mysql -u root <<EOF
+        CREATE DATABASE open3a;
+        CREATE USER 'dewawi'@'localhost' IDENTIFIED BY 'dewawi';
+        GRANT ALL PRIVILEGES ON dewawi . * TO 'dewawi'@'localhost';
+        FLUSH PRIVILEGES;
+EOF
+
+echo "dewawi herunterladen"
+echo "********************************"
+cd /var/www/html
+wget https://github.com/dewawi/dewawi/archive/1.0.1.zip
+unzip 1.0.1.zip
+mv dewawi-1.0.1 dewawi
+
+echo "Zugriffsrechte werden gesetzt"
+echo "*****************************"
+chown -R www-data:www-data /var/www/html/dewawi
+echo
+clear
+echo "*******************************************************************************************"
+echo "dewawi installiert. Bitte ueber das Web die Konfiguration vornehmen"
+echo "weiter gehts mit dem Browser. Gehen Sie auf http://$IP/dewawi/"
+echo "**************************************************************************"
