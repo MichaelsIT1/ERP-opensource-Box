2a7663a00681200bf1cc53ca656667e7c5d76b19
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-07-12 20:55:08 +0200

    Update install-docspell.sh

--- scripts/install-docspell.sh
+++ scripts/install-docspell.sh
@@ -28,19 +28,29 @@ apt install ghostscript tesseract-ocr tesseract-ocr-deu tesseract-ocr-eng unpape
 
 echo "SOLR Installation"
 echo "**********************************"
+#cd /root/
+#wget https://downloads.apache.org/lucene/solr/8.11.2/solr-8.11.2.tgz
+#tar xzf solr-8.11.2.tgz
+#bash solr-8.11.2/bin/install_solr_service.sh solr-8.11.2.tgz
+#systemctl start solr
+#su solr -c '/opt/solr-8.11.2/bin/solr create -c docspell'
+
 cd /root/
-wget https://downloads.apache.org/lucene/solr/8.11.2/solr-8.11.2.tgz
-tar xzf solr-8.11.2.tgz
-bash solr-8.11.2/bin/install_solr_service.sh solr-8.11.2.tgz
-
+wget https://downloads.apache.org/lucene/solr/8.11.1/solr-8.11.1.tgz
+tar xzf solr-8.11.1.tgz
+sleep 5
+bash solr-8.11.1/bin/install_solr_service.sh solr-8.11.1.tgz
+sleep 5
 systemctl start solr
+sleep 5
+su solr -c '/opt/solr-8.11.1/bin/solr create -c docspell'
 
-su solr -c '/opt/solr-8.11.2/bin/solr create -c docspell'
 
 echo "PostgreSQL Installation"
 echo "**********************************"
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg >/dev/null
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ bullseye-pgdg main" > /etc/apt/sources.list.d/postgresql.list'
+sleep 5
 apt update && apt full-upgrade -y
 apt install postgresql-14 -y
 
