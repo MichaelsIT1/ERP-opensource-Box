b17a23fda0f9d58c07ff804d801ee8878cb18283
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-07-15 20:08:19 +0200

    Update install-zammad.sh

--- scripts/install-zammad.sh
+++ scripts/install-zammad.sh
@@ -2,12 +2,6 @@ echo "Zeitzone auf Europe/Berlin gesetzt"
 echo "**********************************"
 timedatectl set-timezone Europe/Berlin 
 echo
-################################ Update your Debian Installation  ###################################################
-# Non-free aktivieren
-tee /etc/apt/sources.list.d/ispconfig.list >/dev/null <<EOF
-deb http://deb.debian.org/debian/ stable main contrib non-free
-deb-src http://deb.debian.org/debian/ stable main contrib non-free
-EOF
 
 echo "Betriebssystem wird aktualisiert"
 echo "***************************************"
