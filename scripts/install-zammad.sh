ca8a3f123141496e2ba850af7b027b56a9e28bdf
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-07-15 21:00:36 +0200

    Update install-zammad.sh

--- scripts/install-zammad.sh
+++ scripts/install-zammad.sh
@@ -29,7 +29,7 @@ echo "*************************************"
 curl -fsSL https://dl.packager.io/srv/zammad/zammad/key | \
   gpg --dearmor | tee /etc/apt/trusted.gpg.d/pkgr-zammad.gpg> /dev/null
   
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/debian 11 main"| \
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/ubuntu 20.04 main"| \
   tee /etc/apt/sources.list.d/zammad.list > /dev/null
   
 apt update -y
