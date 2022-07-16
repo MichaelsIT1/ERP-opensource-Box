92feeaa1aa626efda7671f357bdf179578186564
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2022-07-16 19:40:01 +0200

    Update install-zammad.sh

--- scripts/install-zammad.sh
+++ scripts/install-zammad.sh
@@ -1,3 +1,13 @@
+#!/bin/sh
+# Status: Alpha
+# Nur fuer Test geeignet. Nicht fuer den produktiven Einsatz.
+# getestet auf Ubuntu 20.04 im LXC Container
+
+# System-Varibale
+IP=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
+
+clear
+
 echo "Zeitzone auf Europe/Berlin gesetzt"
 echo "**********************************"
 timedatectl set-timezone Europe/Berlin 
@@ -7,66 +17,32 @@ echo "Betriebssystem wird aktualisiert"
 echo "***************************************"
 apt update -y && apt dist-upgrade -y
 
-# Debian 9, 10 & 11, Ubuntu 16.04, 18.04 & 20.04
-apt install libimlib2 -y
 
-# install PostgresSQL
-apt install postgresql -y
 
-apt install curl apt-transport-https gnupg -y
-
-echo "installing Elasticsearch"
-echo "***************************"
 apt install apt-transport-https sudo wget curl gnupg -y
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/elasticsearch.gpg] https://artifacts.elastic.co/packages/7.x/apt stable main"| \
-  tee -a /etc/apt/sources.list.d/elastic-7.x.list > /dev/null
-curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | \
-gpg --dearmor | tee /etc/apt/trusted.gpg.d/elasticsearch.gpg> /dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/elasticsearch.gpg] https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list > /dev/null
+curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | gpg --dearmor | tee /etc/apt/trusted.gpg.d/elasticsearch.gpg> /dev/null
 apt update -y
 apt install elasticsearch -y
-sleep 5
 /usr/share/elasticsearch/bin/elasticsearch-plugin install ingest-attachment
 
-#systemctl start elasticsearch
-#systemctl enable elasticsearch
+apt install locales -y
+locale-gen en_US.UTF-8
+echo "LANG=en_US.UTF-8" > /etc/default/locale
 
-echo "Add Repository and install Zammad"
-echo "*************************************"
-curl -fsSL https://dl.packager.io/srv/zammad/zammad/key | \
-  gpg --dearmor | tee /etc/apt/trusted.gpg.d/pkgr-zammad.gpg> /dev/null
-  
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/debian 11 main"| \
-  tee /etc/apt/sources.list.d/zammad.list > /dev/null
-  
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/pkgr-zammad.gpg] https://dl.packager.io/srv/deb/zammad/zammad/stable/ubuntu 20.04 main" | tee /etc/apt/sources.list.d/zammad.list > /dev/null
+
+
+echo "install zammad"
 apt update -y
 apt install zammad -y
 
+echo "connect zammad"
 # Set the Elasticsearch server address
 zammad run rails r "Setting.set('es_url', 'http://localhost:9200')"
 
 # Build the search index
 zammad run rake zammad:searchindex:rebuild
-
-# Zammad service to start all services at once
-systemctl restart zammad
-
-# Zammads internal puma server (relevant for displaying the web app)
-systemctl restart) zammad-web
-
-# Zammads background worker - relevant for all delayed- and background jobs
-systemctl restart zammad-worker
-
-# Zammads websocket server for session related information
-systemctl restart zammad-websocket
-
-# Set the Elasticsearch server address
-zammad run rails r "Setting.set('es_url', 'http://localhost:9200')"
-
-# Build the search index
-zammad run rake zammad:searchindex:rebuild
-
-systemctl start elasticsearch
-systemctl enable elasticsearch
 
 echo "*******************************************************************************************"
 echo "zammad erfolgreich installiert. Bitte ueber das Web die Konfiguration vornehmen"
