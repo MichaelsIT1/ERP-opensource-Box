f34284acf20ffc56f54b1d0d9ac94ef7061ce41a
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2023-08-07 18:42:08 +0200

    Update install-debian.sh

--- scripts/install-debian.sh
+++ scripts/install-debian.sh
@@ -4,10 +4,10 @@ timedatectl set-timezone Europe/Berlin
 echo
 ################################ Update your Debian Installation  ###################################################
 # Non-free aktivieren
-tee /etc/apt/sources.list.d/ispconfig.list >/dev/null <<EOF
-deb http://deb.debian.org/debian/ stable main contrib non-free
-deb-src http://deb.debian.org/debian/ stable main contrib non-free
-EOF
+#tee /etc/apt/sources.list.d/ispconfig.list >/dev/null <<EOF
+#deb http://deb.debian.org/debian/ stable main contrib non-free
+#deb-src http://deb.debian.org/debian/ stable main contrib non-free
+#EOF
 
 echo "Betriebssystem wird aktualisiert"
 echo "***************************************"
