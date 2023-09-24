141b2923f725578bc47c2665268210065f80fb76
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2023-09-24 20:09:56 +0200

    Update install-motioneye.sh

--- scripts/install-motioneye.sh
+++ scripts/install-motioneye.sh
@@ -34,7 +34,7 @@ rm get-pip.py
 sleep 5
 python3 -m pip install 'https://github.com/motioneye-project/motioneye/archive/dev.tar.gz'
 sleep 5
-/usr/local/bi/motioneye_init
+/usr/local/bin/motioneye_init
 
 # Upgrade
 sleep 40
