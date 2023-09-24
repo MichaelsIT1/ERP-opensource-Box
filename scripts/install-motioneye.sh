3b14679fb0ad7a6e4aff92f2791646e2e598f2e2
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2023-09-24 19:52:04 +0200

    Update install-motioneye.sh

--- scripts/install-motioneye.sh
+++ scripts/install-motioneye.sh
@@ -18,21 +18,22 @@ apt update -y && apt dist-upgrade -y
 
 apt --no-install-recommends install ca-certificates curl python3 python3-dev libcurl4-openssl-dev gcc libssl-dev -y
 sleep 5
-
+apt install python3-pip -y
 rm /usr/lib/python3.11/EXTERNALLY-MANAGED
 sleep 5
 
-curl -sSfO 'https://bootstrap.pypa.io/get-pip.py'
-sleep 5
-python3 get-pip.py
-sleep 5
-rm get-pip.py
+# PIP installieren
+#curl -sSfO 'https://bootstrap.pypa.io/get-pip.py'
+#sleep 5
+#python3 get-pip.py
+#sleep 5
+#rm get-pip.py
 
 
 sleep 5
 python3 -m pip install 'https://github.com/motioneye-project/motioneye/archive/dev.tar.gz'
 sleep 5
-motioneye_init
+/usr/local/bi/motioneye_init
 
 # Upgrade
 sleep 40
