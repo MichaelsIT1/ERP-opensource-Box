d880a659883649ee214b27364e15c2f3af2d7fb2
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2023-09-30 23:57:49 +0200

    Update install-zammad.sh

--- scripts/install-zammad.sh
+++ scripts/install-zammad.sh
@@ -30,10 +30,14 @@ echo "deb [signed-by=/etc/apt/trusted.gpg.d/pkgr-zammad.gpg] https://dl.packager
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | \
   gpg --dearmor | tee /etc/apt/trusted.gpg.d/elasticsearch.gpg> /dev/null
 
-apt update
-apt install elasticsearch -y
-/usr/share/elasticsearch/bin/elasticsearch-plugin install ingest-attachment
-
+apt install apt-transport-https wget curl gnupg
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/elasticsearch.gpg] https://artifacts.elastic.co/packages/7.x/apt stable main"| \
+  tee -a /etc/apt/sources.list.d/elastic-7.x.list > /dev/null
+curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | \
+  gpg --dearmor | tee /etc/apt/trusted.gpg.d/elasticsearch.gpg> /dev/null
+ apt update
+ apt install elasticsearch -y
+ /usr/share/elasticsearch/bin/elasticsearch-plugin install ingest-attachment
 
 apt install locales -y
 locale-gen en_US.UTF-8
