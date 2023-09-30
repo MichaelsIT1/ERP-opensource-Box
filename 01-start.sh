8bbe8f084d97c2142b45216741185f871bb82dcf
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2023-09-30 22:59:09 +0200

    Update 01-start.sh

--- 01-start.sh
+++ 01-start.sh
@@ -8,7 +8,7 @@ CPW=12345                                                   #Container root-Pass
 
 # Container Images
 DEBIAN10=local:vztmpl/debian-10-standard_10.7-1_amd64.tar.gz
-DEBIAN11=local:vztmpl/debian-11-standard_11.0-1_amd64.tar.gz
+DEBIAN11=local:vztmpl/debian-11-standard_11.7-1_amd64.tar.zst 
 DEBIAN12=local:vztmpl/debian-12-standard_12.0-1_amd64.tar.zst
 UBUNTU=local:vztmpl/ubuntu-22.10-standard_22.10-1_amd64.tar.zst
 
