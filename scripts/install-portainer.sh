4bc30d6c435518db97d180b52bbb05017b7b2b73
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2024-11-27 20:20:46 +0100

    Update install-portainer.sh

--- scripts/install-portainer.sh
+++ scripts/install-portainer.sh
@@ -31,5 +31,5 @@ sleep 5
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 sleep 10
 clear
-echo "weiter gehts mit dem Browser. Gehen Sie auf https://$IP:9433/"
+echo "weiter gehts mit dem Browser. Gehen Sie auf https://$IP:9443/"
 echo "*************************************************************"
