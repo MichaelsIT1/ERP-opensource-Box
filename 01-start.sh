982003b0387c24a5447cf38e5a5f1d619f763031
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2025-12-17 22:28:43 +0100

    Update 01-start.sh

--- 01-start.sh
+++ 01-start.sh
@@ -40,7 +40,7 @@ Info: Alle Container beginnen ab ID900
 5) iTop (CMDB)
 6) i-doit (CMDB)
 7) checkMK-Raw (LAN-Monitoring, debian12)
-8) Nextcloud (Zusammenarbeit, debian12)
+8) Nextcloud (Zusammenarbeit, debian13)
 9) motioneye (Videokamerazentrale, debian12)
 10) Zammad (Ticketsystem, debian12 FEHLER)
 11) Zammad (Ticketsystem, Ubuntu22.04)
