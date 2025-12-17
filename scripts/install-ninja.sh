9723eec0287a967f9c2434baaf2f271bc3294f04
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2025-12-17 23:27:03 +0100

    Update install-ninja.sh

--- scripts/install-ninja.sh
+++ scripts/install-ninja.sh
@@ -92,8 +92,9 @@ chown -R www-data:www-data /var/www/invoice_ninja
 chmod -R 755 /var/www/invoice_ninja
 
 tee /etc/issue >/dev/null <<EOF
-ninja.$(hostname -f); oder
-\4/dashboard
+\4/dashboard oder
+ninja.$(hostname -f) (DNS oder Hostdatei erforderlich)
+
 
 EOF
 
