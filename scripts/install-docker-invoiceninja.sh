2de8a3965ce89a977a382f0fadaf608622d007e9
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2025-12-19 21:34:56 +0100

    Update install-docker-invoiceninja.sh

--- scripts/install-docker-invoiceninja.sh
+++ scripts/install-docker-invoiceninja.sh
@@ -6,7 +6,13 @@
 # System-Varibale
 IP=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
 
+apt update && apt dist-upgrade -y
+sleep 5
+
+apt install docker.io docker-compose git ca-certificates curl gnupg lsb-release -y
+sleep 5
 clear
+
 echo "Invoice Ninja installieren"
 echo "*******************************"
 git clone https://github.com/invoiceninja/dockerfiles.git -b debian
