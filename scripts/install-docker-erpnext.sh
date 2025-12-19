2dc04e3f353b9998e331ec1b99c8ff610955aa47
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2025-12-19 21:26:30 +0100

    Update install-docker-erpnext.sh

--- scripts/install-docker-erpnext.sh
+++ scripts/install-docker-erpnext.sh
@@ -6,9 +6,13 @@
 # System-Varibale
 IP=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
 
+apt update && apt dist-upgrade -y
+sleep 5
+
 apt install docker.io docker-compose git ca-certificates curl gnupg lsb-release -y
 sleep 5
 clear
+
 echo "ERPnext installieren"
 echo "*******************************"
 git clone https://github.com/frappe/frappe_docker
