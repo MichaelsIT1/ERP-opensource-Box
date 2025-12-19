50752af401f02cbb9f16f23e418513fa09306ede
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2025-12-19 21:34:38 +0100

    Update install-docker-docspell.sh

--- scripts/install-docker-docspell.sh
+++ scripts/install-docker-docspell.sh
@@ -8,7 +8,13 @@
 # System-Varibale
 IP=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
 
+apt update && apt dist-upgrade -y
+sleep 5
+
+apt install docker.io docker-compose git ca-certificates curl gnupg lsb-release -y
+sleep 5
 clear
+
 echo "docspell installieren"
 echo "*******************************"
 git clone https://github.com/eikek/docspell
