0fdeebd1aa0f2510a9ffa884e9efab00654c1ebe
MichaelsIT1 <98195192+MichaelsIT1@users.noreply.github.com>
2025-12-20 19:35:21 +0100

    Create install-docker-netalertx.sh

---
+++ scripts/install-docker-netalertx.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Status: Alpha
+# Nur fuer Test geeignet. Nicht fuer den produktiven Einsatz.
+
+
+# System-Varibale
+IP=$(ip addr show eth0 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
+
+apt update && apt dist-upgrade -y
+sleep 5
+
+apt install docker.io docker-compose git ca-certificates curl gnupg lsb-release -y
+sleep 5
+clear
+
+echo "ERPNext installieren"
+echo "*******************************"
+git clone https://github.com/frappe/frappe_docker
+cd frappe_docker
+docker compose -f pwd.yml up -d
+
+tee /etc/issue >/dev/null <<EOF
+\4:8080
+
+
+EOF
+
+clear
+
+echo "weiter gehts mit dem Browser. Gehen Sie auf http://$IP:8080"
+echo "Username: Administrator"
+echo "Password: admin"
+echo "*************************************************************"
